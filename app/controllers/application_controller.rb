ec69ae1da859cdb4c0561f22fe336d2f678f49d7
tyrro <rajib.das.shuva@gmail.com>
2022-08-31 14:32:50 +0700

    feat: Implement multi-tenancy skeleton with acts_as_tenant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  set_current_tenant_through_filter
+
+  before_action :find_current_tenant
+
+  def find_current_tenant
+    return unless current_user
+
+    current_company = current_user.company
+    set_current_tenant(current_company)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_105026) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_170909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +28,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_105026) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "companies"
 end
